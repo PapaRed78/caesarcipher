58dcda8c0137f8354a1506cf8f9854767b77fa39
PapaRed78 <you@example.com>
2026-04-07 12:59:08 -0400

    exercise 2.1

---
+++ 2.1/caesar.js
@@ -0,0 +1,21 @@
+// Caesar Cipher Implementation Question Two
+
+// let partyLocation = "GARDEN"; // String
+// let shiftValue = 3; // Number
+// let encryptedMessage = ""; // String
+// let decryptedMessage = ""; // String
+// let isPartySafe = false; // Boolean
+
+// Variable Declarations Question Three
+
+// let partyLocation = "GARDEN"; // As the message might change.
+// const shiftValue = 3; // Since the shift value remains constant throughout.
+// let encryptedMessage = ""; // This will change when we encode the message with the original party location.
+// let decryptedMessage = ""; // This will change when we decode the encrypted message to reveal the party location.
+// let isPartySafe = false; // Hopefully, this doesn't change.
+
+// Data Types Question Four
+
+// const shiftValue = 3;
+// Number.isInteger(shiftValue);
+// true because 3  is a Number and an integer.
